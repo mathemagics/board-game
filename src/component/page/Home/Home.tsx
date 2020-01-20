be5c2d563b8597a2a1d18605856e0e8185fb2135
Paul <paul.bodnar@metalab.co>
2020-01-19 16:19:45 -0800

    add react router, babel module resolver

--- src/component/page/Home/Home.tsx
+++ src/component/page/Home/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-export default ({children}: {children: React.ReactNode}) => (
+export default () => (
   <div>Home</div>
 );
--- src/component/root/App.tsx
+++ src/component/root/App.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 import {ApolloProvider} from '@apollo/react-hooks';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Character from 'component/page/Character';
+import Home from 'component/page/Home';
+
 
 import client from './apollo';
 
 export default () => (
   <ApolloProvider client={client}>
-    <div>Yellllllow</div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/character">
+          <Character />
+        </Route>
+      </Switch>
+    </Router>
   </ApolloProvider>
 );
