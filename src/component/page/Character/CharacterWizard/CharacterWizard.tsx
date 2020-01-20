80aab1cf99d69da1ac59c4f232d20fba6f4435fb
Paul <paul.bodnar@metalab.co>
2020-01-19 19:19:25 -0800

    add unform

--- src/component/page/Character/CharacterWizard/CharacterWizard.tsx
+++ src/component/page/Character/CharacterWizard/CharacterWizard.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
+import {Form, Input} from '@rocketseat/unform';
+
+const onBlur = (e: React.FormEvent<HTMLInputElement>) => {
+  // graphql call
+  console.log((e.target as HTMLInputElement).value);
+};
 
 export default () => (
-  <div>CharacterWizard</div>
-);
+  <Form onSubmit={onBlur}>
+    <div>
+      <div>name</div>
+      <Input name="name" onBlur={onBlur} />
+    </div>
+    <div>
+      <div>species</div>
+      <Input name="species" onBlur={onBlur} />
+    </div>
+    <div>
+      <button type="submit">Next</button>
+    </div>
+  </Form>
+)
