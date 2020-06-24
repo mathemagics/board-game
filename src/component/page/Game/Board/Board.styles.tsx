681402b543ce772909b5b16fff19b075a10ffb8d
Paul <paul.bodnar@metalab.co>
2020-06-23 19:22:46 -0600

    colurz

--- src/component/page/Game/Board/Board.styles.tsx
+++ src/component/page/Game/Board/Board.styles.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import { Layout, Hexagon } from "react-hexgrid";
 
+const DEFAULT_COLOR = "#15788c";
+const STARTING_COLOR = "#59ACC2";
+const DRAW_COLOR = "#25B49A";
+
 export const Hex = styled(Hexagon)`
   background: white;
   fill: ${({ starting, drawing }) =>
-    starting ? "#44a955" : drawing ? "#4d44a9" : "#4499a9"};
+    starting ? STARTING_COLOR : drawing ? DRAW_COLOR : DEFAULT_COLOR};
 `;
 
 export const Map = styled(Layout)`
